9b71eca6c9ebcd0ddc0b86ed63555d314509f9a4
Desouky <alaa.desouky33@gmail.com>
2022-04-16 21:29:23 +0200

    User model

---
+++ src/models/user.js
@@ -0,0 +1,71 @@
+import { Model, DataTypes } from 'sequelize'
+import bcrypt from 'bcrypt'
+import environment from '../config/environment'
+
+export default (sequelize) => {
+  class User extends Model {
+    static associate(models) { }
+
+    // Password Handling
+    static async hashPassword(password) {
+      return bcrypt.hash(password, environment.saltRounds)
+    }
+
+    static async comparePassword(password, hashedPassword) {
+      return bcrypt.compare(password, hashedPassword)
+    }
+  }
+
+  // User fields
+  User.init({
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: {
+          msg: "Not a valid email address"
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING(50),
+      unique: true,
+    },
+    firstName: {
+      type: DataTypes.STRING(50),
+      validate: {
+        len: {
+          args: [0, 50],
+          msg: 'First name has too many characters'
+        }
+      }
+    },
+    lastName: {
+      type: DataTypes.STRING(50),
+      validate: {
+        len: {
+          args: [0, 50],
+          msg: 'Last name has too many characters'
+        }
+      }
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+    indexes: [{ unique: true, fields: ['email'] }]
+  })
+
+  // Password Hashing
+  User.beforeSave(async (user, options) => {
+    const hashedPassword = await User.hashPassword(user.password)
+    user.password = hashedPassword
+  })
+
+  // Return
+  return User
+}
