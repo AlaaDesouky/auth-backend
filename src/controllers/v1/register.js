a2e35c0b4d219d970726ca9a3fc3af1af3efa0db
AlaaDesouky <alaa.desouky33@gmail.com>
2022-04-17 22:24:48 +0200

    Added transactions to sequelize

--- src/controllers/v1/register.js
+++ src/controllers/v1/register.js
@@ -4,7 +4,7 @@ import JWTUtils from '../../utils/jwt-utils'
 import runAsyncWrapper from '../../utils/runAsyncWrapper'
 
 const router = Router()
-const { User, Role } = models
+const { User, Role, sequelize } = models
 
 router.post('/register', runAsyncWrapper(async (req, res) => {
   const { email, password, roles } = req.body
@@ -16,25 +16,30 @@ router.post('/register', runAsyncWrapper(async (req, res) => {
 
   // Create new user
   try {
-    const newUser = await User.create({ email, password })
-    // Generate jwt tokens
-    const jwtPayload = { email }
-    const accessToken = JWTUtils.generateAccessToken(jwtPayload)
-    const refreshToken = JWTUtils.generateRefreshToken(jwtPayload)
-    // Create refresh token model, -- a mixin provided by sequelize when two models are associated --
-    await newUser.createRefreshToken({ token: refreshToken })
+    const result = await sequelize.transaction(async () => {
+      const newUser = await User.create({ email, password })
+      // Generate jwt tokens
+      const jwtPayload = { email }
+      const accessToken = JWTUtils.generateAccessToken(jwtPayload)
+      const refreshToken = JWTUtils.generateRefreshToken(jwtPayload)
+      // Create refresh token model, -- a mixin provided by sequelize when two models are associated --
+      await newUser.createRefreshToken({ token: refreshToken })
 
-    // Create and associate roles
-    if (roles && Array.isArray(roles)) {
-      const rolesToSave = []
-      // -- 'for of loop' awaits for an array of promises --
-      for (const role of roles) {
-        const newRole = await Role.create({ role })
-        rolesToSave.push(newRole)
+      // Create and associate roles
+      if (roles && Array.isArray(roles)) {
+        const rolesToSave = []
+        // -- 'for of loop' awaits for an array of promises --
+        for (const role of roles) {
+          const newRole = await Role.create({ role })
+          rolesToSave.push(newRole)
+        }
+        // -- a mixin provided by sequelize --
+        await newUser.addRoles(rolesToSave)
       }
-      // -- a mixin provided by sequelize --
-      await newUser.addRoles(rolesToSave)
-    }
+
+      return { accessToken, refreshToken }
+    })
+    const { accessToken, refreshToken } = result
 
     return res.status(201).send({
       success: true,
