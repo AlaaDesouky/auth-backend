111941622ba77f12c427bcdd3fd5f29f2e2b236b
AlaaDesouky <alaa.desouky33@gmail.com>
2022-04-17 16:09:48 +0200

    Require auth middleware

---
+++ src/middleware/requiresAuth.js
@@ -0,0 +1,37 @@
+import JWTUtils from '../utils/jwt-utils'
+
+function requireAuth(tokenType = 'accessToken') {
+  return function (req, res, next) {
+    const authHeader = req.headers.authorization
+
+    if (authHeader) {
+      try {
+        var [bearer, token] = authHeader.split(' ')
+        if (bearer.toLowerCase() !== 'bearer' || !token) {
+          throw Error
+        }
+      } catch (error) {
+        return res.status(401).send({ success: false, message: 'Bearer token malformed' })
+      }
+    } else {
+      return res.status(401).send({ success: false, message: 'Authorization header not found' })
+    }
+
+    try {
+      let jwt
+      switch (tokenType) {
+        case 'refreshToken':
+          jwt = JWTUtils.verifyRefreshToken(token)
+
+        case 'accessToken':
+        default:
+          jwt = JWTUtils.verifyAccessToken(token)
+      }
+
+      req.body.jwt = jwt
+      next()
+    } catch (error) {
+      return res.status(401).send({ success: false, message: 'Invalid token' })
+    }
+  }
+}
