ecebef6f7cb715eddea500d0b60a7664b7cfff16
AlaaDesouky <alaa.desouky33@gmail.com>
2022-04-18 14:43:52 +0200

    Test for errors middleware

--- tests/controllers/v1/token.test.js
+++ tests/controllers/v1/token.test.js
@@ -1,5 +1,6 @@
 import request from 'supertest'
 import models from '../../../src/models'
+import JWTUtils from '../../../src/utils/jwt-utils';
 import TestHelpers from '../../tests-helpers'
 
 describe('token', () => {
@@ -48,6 +49,24 @@ describe('token', () => {
         .expect(401)
       expect(response.body.success).toEqual(false)
       expect(response.body.message).toEqual('Bearer token malformed')
+    })
+
+    describe('errors middleware', () => {
+      it('should return 500 if something went wrong', async () => {
+        const jwtUtilsSpy = jest.spyOn(JWTUtils, 'generateAccessToken')
+        jwtUtilsSpy.mockImplementation(() => {
+          throw Error('test error')
+        })
+        const refreshToken = newUserResponse.body.data.refreshToken
+        const response = await request(app)
+          .post('/v1/token')
+          .set('Authorization', `Bearer ${refreshToken}`)
+          .send()
+          .expect(500)
+        jwtUtilsSpy.mockRestore()
+        expect(response.body.success).toEqual(false)
+        expect(response.body.message).toEqual('test error')
+      })
     })
 
     it('should get a new access token successfully', async () => {
